2e0c774a87d336dc6b61710888bf6d604a764423
Yokoyama <x.xiao.turquoise@gmail.com>
2017-03-20 22:31:37 +0900

    メソッド呼び出し処理を修正と追加

--- lib/stock_fundamental_scraper/rakuten_sec.rb
+++ lib/stock_fundamental_scraper/rakuten_sec.rb
@@ -1,135 +1,137 @@
 # 楽天証券提供のデータをスクレイピング
-class RakutenSecPL
-  def initialize(company_fs)
-    @company_fs = company_fs
-  end
-
-  # PL
-  def pl
-    pl_node_set = @company_fs.search('//*[@id="str-main"]/table[3]')
-    pl_ary = pl_node_set.children.children.children.map(&:text)
-    pl_table = pl_ary.each_slice(5).to_a
-
-    header = pl_table[0]
-    pl_table.each_with_object([]).with_index do |(row, result),i|
-      next if i == 0
-
-      summary = {}
-      row.each_with_index do |value, index|
-        if index == 0
-          summary[:summary_name] = value
-        else
-          if summary[:values].nil?
-            summary[:values] = []
-          end
-          summary[:values] << {
-              term: header[index],
-              value: value
-          }
-        end
-      end
-      result << summary
-    end
-  end
-end
-
-class RakutenSecBS
-  def initialize(company_fs)
-    @company_fs = company_fs
-  end
-
-  # BS
-  def bs
-    bs_node_set = @company_fs.search('//*[@id="str-main"]/table[3]')
-    bs_array = bs_node_set[0].children.children.children.map(&:text)
-    bs_table = bs_array.each_slice(5).to_a
-
-    header = bs_table[0]
-
-    cash_start_index = bs_table.index { |row| row.include?("資産の部") }
-    dept_start_index = bs_table.index { |row| row.include?("負債の部") }
-    capital_start_index = bs_table.index { |row| row.include?("資本の部") }
-
-    result = {
-        cash_category: {
-            name: "資産の部",
-            summaries: []
-        },
-        dept_category: {
-            name: "負債の部",
-            summaries: []
-        },
-        capital_categpry: {
-            name: "資本の部",
-            summaries: []
-        }
-    }
-
-    bs_table.each_with_index do |row, index|
-      next if [cash_start_index, dept_start_index, capital_start_index].include?(index)
-
-      summary = {}
-      row.each_with_index do |value, row_index|
-        if row_index == 0
-          summary[:summary_name] = value
-        else
-          if summary[:values].nil?
-            summary[:values] = []
-          end
-          summary[:values] << {
-              term: header[index],
-              value: value
-          }
-        end
-      end
-
-      if index > cash_start_index && dept_start_index > index
-        # 資産の部
-        result[:cash_category][:summaries] << summary
-      elsif index > dept_start_index && capital_start_index > index
-        # 負債の部
-        result[:dept_category][:summaries] << summary
-      else
-        # 資本の部
-        result[:capital_categpry][:summaries] << summary
-      end
+module RakutenSec
+  class PL
+    def initialize(company_fs)
+      @company_fs = company_fs
     end
 
-    result
-  end
-end
+    # PL
+    def pl
+      pl_node_set = @company_fs.search('//*[@id="str-main"]/table[3]')
+      pl_ary = pl_node_set.children.children.children.map(&:text)
+      pl_table = pl_ary.each_slice(5).to_a
 
-class RakutenSecCF
-  def initialize(company_fs)
-    @company_fs = company_fs
-  end
+      header = pl_table[0]
+      pl_table.each_with_object([]).with_index do |(row, result),i|
+        next if i == 0
 
-  # CF
-  def cf
-    cf_node_set = @company_fs.search('//*[@id="str-main"]/table[3]')
-
-    cf_array = cf_node_set[0].children.children.children.map(&:text)
-    cf_table = cf_array.each_slice(5).to_a
-
-    header = cf_table[0]
-    cf_table.each_with_object([]).with_index do |(row, result),i|
-      next if i == 0
-
-      summary = {}
-      row.each_with_index do |value, index|
-        if index == 0
-          summary[:summary_name] = value
-        else
-          if summary[:values].nil?
-            summary[:values] = []
+        summary = {}
+        row.each_with_index do |value, index|
+          if index == 0
+            summary[:summary_name] = value
+          else
+            if summary[:values].nil?
+              summary[:values] = []
+            end
+            summary[:values] << {
+                term: header[index],
+                value: value
+            }
           end
-          summary[:values] << {
-              term: header[index],
-              value: value
+        end
+        result << summary
+      end
+    end
+  end
+
+  class BS
+    def initialize(company_fs)
+      @company_fs = company_fs
+    end
+
+    # BS
+    def bs
+      bs_node_set = @company_fs.search('//*[@id="str-main"]/table[3]')
+      bs_array = bs_node_set[0].children.children.children.map(&:text)
+      bs_table = bs_array.each_slice(5).to_a
+
+      header = bs_table[0]
+
+      cash_start_index = bs_table.index { |row| row.include?("資産の部") }
+      dept_start_index = bs_table.index { |row| row.include?("負債の部") }
+      capital_start_index = bs_table.index { |row| row.include?("資本の部") }
+
+      result = {
+          cash_category: {
+              name: "資産の部",
+              summaries: []
+          },
+          dept_category: {
+              name: "負債の部",
+              summaries: []
+          },
+          capital_categpry: {
+              name: "資本の部",
+              summaries: []
           }
+      }
+
+      bs_table.each_with_index do |row, index|
+        next if [cash_start_index, dept_start_index, capital_start_index].include?(index)
+
+        summary = {}
+        row.each_with_index do |value, row_index|
+          if row_index == 0
+            summary[:summary_name] = value
+          else
+            if summary[:values].nil?
+              summary[:values] = []
+            end
+            summary[:values] << {
+                term: header[index],
+                value: value
+            }
+          end
+        end
+
+        if index > cash_start_index && dept_start_index > index
+          # 資産の部
+          result[:cash_category][:summaries] << summary
+        elsif index > dept_start_index && capital_start_index > index
+          # 負債の部
+          result[:dept_category][:summaries] << summary
+        else
+          # 資本の部
+          result[:capital_categpry][:summaries] << summary
         end
       end
-      result << summary
+
+      result
+    end
+  end
+
+  class CF
+    def initialize(company_fs)
+      @company_fs = company_fs
+    end
+
+    # CF
+    def cf
+      cf_node_set = @company_fs.search('//*[@id="str-main"]/table[3]')
+
+      cf_array = cf_node_set[0].children.children.children.map(&:text)
+      cf_table = cf_array.each_slice(5).to_a
+
+      header = cf_table[0]
+      cf_table.each_with_object([]).with_index do |(row, result),i|
+        next if i == 0
+
+        summary = {}
+        row.each_with_index do |value, index|
+          if index == 0
+            summary[:summary_name] = value
+          else
+            if summary[:values].nil?
+              summary[:values] = []
+            end
+            summary[:values] << {
+                term: header[index],
+                value: value
+            }
+          end
+        end
+        result << summary
+      end
     end
   end
 end
--- lib/stock_fundamental_scraper/scraper.rb
+++ lib/stock_fundamental_scraper/scraper.rb
@@ -2,6 +2,8 @@ require 'mechanize'
 require 'pry'
 require 'stock_fundamental_scraper/kabu_tec'
 require 'stock_fundamental_scraper/rakuten_sec'
+require 'stock_fundamental_scraper/kabu_sensor'
+require 'stock_fundamental_scraper/kabu_tan'
 
 module StockFundamentalScraper
   class Scraper
@@ -20,15 +22,14 @@ module StockFundamentalScraper
 
       if rakuten_sec
         # 楽天証券データ取得用インスタンス
-        @rakuten_sec_pl = RakutenSecPL.new(scrape_page("https://www.trkd-asia.com/rakutensec/quote.jsp?ric=#{stock_code}.T&c=ja&ind=2&fs=1"))
+        @rakuten_sec_pl = RakutenSec::PL.new(scrape_page("https://www.trkd-asia.com/rakutensec/quote.jsp?ric=#{stock_code}.T&c=ja&ind=2&fs=1"))
         ## 同ドメインサイトへのアクセスなので5秒のインターバルをおく
         sleep(3)
-        @rakuten_sec_bs = RakutenSecBS.new(scrape_page("https://www.trkd-asia.com/rakutensec/quote.jsp?ric=#{stock_code}.T&c=ja&ind=2&fs=2"))
+        @rakuten_sec_bs = RakutenSec::BS.new(scrape_page("https://www.trkd-asia.com/rakutensec/quote.jsp?ric=#{stock_code}.T&c=ja&ind=2&fs=2"))
         ## 同ドメインサイトへのアクセスなので5秒のインターバルをおく
         sleep(3)
-        @rakuten_sec_cf = RakutenSecCF.new(scrape_page("https://www.trkd-asia.com/rakutensec/quote.jsp?ric=#{stock_code}.T&c=ja&ind=2&fs=3"))
+        @rakuten_sec_cf = RakutenSec::CF.new(scrape_page("https://www.trkd-asia.com/rakutensec/quote.jsp?ric=#{stock_code}.T&c=ja&ind=2&fs=3"))
       end
-
 
       if kabu_tan
         # 株探データ取得用インスタンス
@@ -38,117 +39,190 @@ module StockFundamentalScraper
 
       if kabu_sensor
         # ボリンジャー(10日)買いシグナル1P
-        @kabu_senser_boli_buy_10_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=1&t_cd=1&s_cd=1"))
+        @kabu_sensor_boli_buy_10_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=1&t_cd=1&s_cd=1"))
         sleep(2)
 
         # ボリンジャー(10日)買いシグナル2P
-        @kabu_senser_boli_buy_10_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=2&t_cd=1&s_cd=1"))
+        @kabu_sensor_boli_buy_10_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=2&t_cd=1&s_cd=1"))
         sleep(2)
 
         # ボリンジャー(10日)売りシグナル1P
-        @kabu_senser_boli_sell_10_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=1&t_cd=1&s_cd=1"))
+        @kabu_sensor_boli_sell_10_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=1&t_cd=1&s_cd=1"))
         sleep(2)
 
         # ボリンジャー(10日)売りシグナル2P
-        @kabu_senser_boli_sell_10_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=2&t_cd=1&s_cd=1"))
+        @kabu_sensor_boli_sell_10_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=2&t_cd=1&s_cd=1"))
         sleep(2)
 
         # ボリンジャー(25日)買いシグナル1P
-        @kabu_senser_boli_buy_10_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=1&t_cd=1&s_cd=2"))
+        @kabu_sensor_boli_buy_25_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=1&t_cd=1&s_cd=2"))
         sleep(2)
 
         # ボリンジャー(25日)買いシグナル2P
-        @kabu_senser_boli_buy_10_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=2&t_cd=1&s_cd=2"))
+        @kabu_sensor_boli_buy_25_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=2&t_cd=1&s_cd=2"))
         sleep(2)
 
         # ボリンジャー(25日)売りシグナル1P
-        @kabu_senser_boli_sell_10_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=1&t_cd=1&s_cd=2"))
+        @kabu_sensor_boli_sell_25_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=1&t_cd=1&s_cd=2"))
         sleep(2)
 
         # ボリンジャー(25日)売りシグナル2P
-        @kabu_senser_boli_sell_10_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=2&t_cd=1&s_cd=2"))
+        @kabu_sensor_boli_sell_25_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=2&t_cd=1&s_cd=2"))
         sleep(2)
 
         # 乖離率 買いシグナル1P
-        @kabu_senser_kairi_buy_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=1&t_cd=5&s_cd=1"))
+        @kabu_sensor_kairi_buy_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=1&t_cd=5&s_cd=1"))
         sleep(2)
 
         # 乖離率 買いシグナル2P
-        @kabu_senser_kairi_buy_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=2&t_cd=5&s_cd=1"))
+        @kabu_sensor_kairi_buy_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=2&t_cd=5&s_cd=1"))
         sleep(2)
 
         # 乖離率 売りシグナル1P
-        @kabu_senser_kairi_sell_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=1&t_cd=5&s_cd=1"))
+        @kabu_sensor_kairi_sell_1 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=1&t_cd=5&s_cd=1"))
         sleep(2)
 
         # 乖離率 売りシグナル2P
-        @kabu_senser_kairi_sell_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=2&t_cd=5&s_cd=1"))
+        @kabu_sensor_kairi_sell_2 = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=2&t_cd=5&s_cd=1"))
         sleep(2)
 
         # サイコロジカルサイン 買いシグナル
-        @kabu_senser_psycho_buy = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=1&t_cd=4&s_cd=1"))
+        @kabu_sensor_psycho_buy = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?page=1&t_cd=4&s_cd=1"))
         sleep(2)
 
         # サイコロジカルサイン 売りシグナル
-        @kabu_senser_psycho_sell = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=1&t_cd=4&s_cd=1"))
+        @kabu_sensor_psycho_sell = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?page=1&t_cd=4&s_cd=1"))
         sleep(2)
 
         # RSI 買いシグナル
-        @kabu_senser_rsi_buy = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?t_cd=6&s_cd=1"))
+        @kabu_sensor_rsi_buy = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?t_cd=6&s_cd=1"))
         sleep(2)
 
         # RSI 売りシグナル
-        @kabu_senser_rsi_sell = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?t_cd=6&s_cd=1"))
+        @kabu_sensor_rsi_sell = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?t_cd=6&s_cd=1"))
         sleep(2)
 
         # MACD 買いシグナル
-        @kabu_senser_macd_buy = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?t_cd=10&s_cd=1"))
+        @kabu_sensor_macd_buy = KabuSensor.new(scrape_page("http://kabusensor.com/signal/kai/?t_cd=10&s_cd=1"))
         sleep(2)
 
         # MACD 売りシグナル
-        @kabu_senser_macd_sell = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?t_cd=10&s_cd=1"))
+        @kabu_sensor_macd_sell = KabuSensor.new(scrape_page("http://kabusensor.com/signal/uri/?t_cd=10&s_cd=1"))
         sleep(2)
 
         # 買いシグナル多数
-        @kabu_senser_buy_signal = KabuSensor.new(scrape_page("http://kabusensor.com/ranking/kaisignal/"))
+        @kabu_sensor_buy_signal = KabuSensor.new(scrape_page("http://kabusensor.com/ranking/kaisignal/"))
         sleep(2)
 
         # 売りシグナル多数
-        @kabu_senser_buy_signal = KabuSensor.new(scrape_page("http://kabusensor.com/ranking/urisignal/"))
+        @kabu_sensor_sell_signal = KabuSensor.new(scrape_page("http://kabusensor.com/ranking/urisignal/"))
       end
-
-      ##
-
-      ##
-      ##
-      ##
-      ##
-      ##
-      ##
-      ##
-      ##
-
-
-
     end
 
-
-    def kabu_tec
-      @kabutec
+    # 株テク：株価情報
+    def kabu_tec_stock_info
+      @kabutec.stock_info
     end
 
+    # 株テク：財務指標
+    def kabu_tec_financial_statement
+      @kabutec.financial_statement
+    end
+
+    # 株テク：自動売買総合診断
+    def kabu_tec_auto_trace_judgment
+      @kabutec.auto_trace_judgment
+    end
+
+    # 楽天証券：PL
     def rakuten_sec_pl
       @rakuten_sec_pl.pl
     end
 
+    # 楽天証券：BS
     def rakuten_sec_bs
       @rakuten_sec_bs.bs
     end
 
+    # 楽天証券：CF
     def rakuten_sec_cf
       @rakuten_sec_cf.cf
     end
 
+    # 株探：本日活況銘柄
+    def kabu_tan_briskness
+      @kabutan_briskness.get_data
+    end
+
+    # 株センサー：ボリンジャー10日買いシグナル
+    def kabu_sensor_bollinger_ten_buy
+      @kabu_sensor_boli_buy_10_1.get_data + @kabu_sensor_boli_buy_10_2.get_data
+    end
+
+    # 株センサー：ボリンジャー10日売りシグナル
+    def kabu_sensor_bollinger_ten_sell
+      @kabu_sensor_boli_sell_10_1.get_data + @kabu_sensor_boli_sell_10_2.get_data
+    end
+
+    # 株センサー：ボリンジャー25日買いシグナル
+    def kabu_sensor_bollinger_twentyfive_buy
+      @kabu_sensor_boli_buy_25_1.get_data + @kabu_sensor_boli_buy_25_2.get_data
+    end
+
+    # 株センサー：ボリンジャー25日売りシグナル
+    def kabu_sensor_bollinger_twentyfive_sell
+      @kabu_sensor_boli_sell_25_1.get_data + @kabu_sensor_boli_sell_25_2.get_data
+    end
+
+    # 株センサー：乖離率 買いシグナル
+    def kabu_sensor_kairi_buy
+      @kabu_sensor_kairi_buy_1.get_data + @kabu_sensor_kairi_buy_2.get_data
+    end
+    
+    # 株センサー：乖離率 売りシグナル
+    def kabu_sensor_kairi_sell
+      @kabu_sensor_kairi_sell_1.get_data + @kabu_sensor_kairi_sell_2.get_data
+    end
+
+    # 株センサー：サイコロジカルサイン 買いシグナル
+    def kabu_sensor_psycho_buy
+      @kabu_sensor_psycho_buy.get_data
+    end
+    
+    # 株センサー：サイコロジカルサイン 売りシグナル
+    def kabu_sensor_psycho_sell
+      @kabu_sensor_psycho_sell.get_data
+    end
+
+    # 株センサー：RSI 買いシグナル
+    def kabu_sensor_rsi_buy
+      @kabu_sensor_rsi_buy.get_data
+    end
+
+    # 株センサー：RSI 売りシグナル
+    def kabu_sensor_rsi_sell
+      @kabu_sensor_rsi_sell.get_data
+    end
+
+    # 株センサー：MACD 買いシグナル
+    def kabu_sensor_macd_buy
+      @kabu_sensor_macd_buy.get_data
+    end
+
+    # 株センサー：MACD 売りシグナル
+    def kabu_sensor_macd_sell
+      @kabu_sensor_macd_sell.get_data
+    end
+
+    # 株センサー：買いシグナル多数
+    def kabu_sensor_buy_signal
+      @kabu_sensor_buy_signal.get_data
+    end
+
+    # 株センサー：
+    def kabu_sensor_sell_signal
+      @kabu_sensor_sell_signal.get_data
+    end
 
     private
 
